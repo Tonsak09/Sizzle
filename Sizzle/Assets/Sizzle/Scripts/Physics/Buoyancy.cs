bad2b3c644e36d21b73a1fe64adeff07e7bdc21f
Narai Risser <89745834+NGR7989@users.noreply.github.com>
2022-08-29 10:21:46 -0400

    more on the math library

--- Sizzle/Assets/Sizzle/Scripts/Physics/Buoyancy.cs
+++ Sizzle/Assets/Sizzle/Scripts/Physics/Buoyancy.cs
@@ -6,12 +6,12 @@ public class Buoyancy : MonoBehaviour
 {
 
     [SerializeField] float maxBuoyancyForce;
+    [SerializeField] float fallForce;
     [SerializeField] float depthBeforeSubmerged;
     //[SerializeField] float fallForce;
     [SerializeField] LayerMask layer;
     [SerializeField] AnimationCurve buoyancyForceCurve;
 
-    private RaycastHit hit;
     private Rigidbody rb;
 
 
@@ -31,13 +31,23 @@ public class Buoyancy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Physics.Raycast(this.transform.position, Vector3.down, out hit, 100, layer);
-        float disFromGround = Vector3.Distance(this.transform.position, hit.point);
-        float forceModifier = buoyancyForceCurve.Evaluate(1 - Mathf.Clamp01(disFromGround / depthBeforeSubmerged)) * maxBuoyancyForce;
-        rb.AddForce(Vector3.up * Mathf.Abs(Physics.gravity.y) * forceModifier);
+        RaycastHit hit;
+        Physics.Raycast(this.transform.position, Vector3.down, out hit, depthBeforeSubmerged, layer);
+        print(hit.point);
+        if(hit.point != null)
+        {
+            float disFromGround = Vector3.Distance(this.transform.position, hit.point);
+            float forceModifier = buoyancyForceCurve.Evaluate(1 - Mathf.Clamp01(disFromGround / depthBeforeSubmerged)) * maxBuoyancyForce;
+            rb.AddForce(Vector3.up * forceModifier, ForceMode.Acceleration);
+        }    
+        else
+        {
+            rb.AddForce(Vector3.down * fallForce, ForceMode.Acceleration);
+        }
+
     }
 
-    private void UpdateRayCheck()
+    /*private void UpdateRayCheck()
     {
         if(Physics.Raycast(this.transform.position, Vector3.down, out hit, 100, layer))
         {
@@ -46,16 +56,16 @@ public class Buoyancy : MonoBehaviour
             
             
             // Not falling
-            /*if (disFromGround <= maxDisFromGround)
+            *//*if (disFromGround <= maxDisFromGround)
             {
                 float xValue = disFromGround / maxBuoyancyForce;
 
                 float force = buoyancyForceCurve.Evaluate(xValue) * maxBuoyancyForce;
                 print(force);
                 rb.AddForce(force * Vector3.up, ForceMode.Acceleration);
-            }*/
+            }*//*
         }
-    }
+    }*/
 
     private void OnDrawGizmos()
     {
