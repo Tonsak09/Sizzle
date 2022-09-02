3700eff22ecd680c13171e3532eefaa8823b221c
Narai Risser <naraiguy@gmail.com>
2022-09-02 15:27:38 -0400

    Jaw opens and closes

--- Sizzle/Assets/Sizzle/Scripts/Sizzle/Grab.cs
+++ Sizzle/Assets/Sizzle/Scripts/Sizzle/Grab.cs
@@ -4,92 +4,96 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
-    /*
-    public KeyCode grabKey;
-    public float range;
-
-    public float grabSpeed;
-    public float throwSpeed;
-    public Vector3 throwOffset;
-
-    public Transform grabPoint;
-    [SerializeField] Transform boneReference;
-
-    private Candle[] candles;
-    private Candle heldCandle;
-
-    public bool candleHeld { get { return heldCandle != null; } }
 
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Neck")]
+    [SerializeField] ConfigurableJoint neckJoint;
+
+    [SerializeField] Vector3 neckDefaultRot;
+    [SerializeField] Vector3 neckTargetRot;
+
+    [SerializeField] AnimationCurve neckOpenAnimCurve;
+    [SerializeField] float neckOpenSpeed;
+
+    [SerializeField] AnimationCurve neckCloseAnimCurve;
+    [SerializeField] float neckCloseSpeed;
+
+    [Header("Jaw")]
+    [SerializeField] ConfigurableJoint jawJoint;
+
+    [SerializeField] Vector3 jawDefaultRot;
+    [SerializeField] Vector3 jawTargetRot;
+
+    [SerializeField] AnimationCurve jawOpenAnimCurve;
+    [SerializeField] float JawOpenSpeed;
+
+    [SerializeField] AnimationCurve jawCloseAnimCurve;
+    [SerializeField] float JawCloseSpeed;
+
+    [Header("Grabbing")]
+    [Tooltip("The threshold the lerp must surpass before Sizzle can grab anything")]
+    [SerializeField] float jawLerpThreshold;
+
+    [SerializeField] Vector3 detectStartOffset;
+    [SerializeField] Vector3 detectTargetOffset;
+    [SerializeField] AnimationCurve detectOffsetAnimCurve;
+    [SerializeField] float detectOffsetSpeed;
+
+    [SerializeField] Vector3 detectStartSize;
+    [SerializeField] Vector3 detectTargetSize;
+
+
+    // The lerp between a mouth open and closed 
+    private float neckLerp;
+    private float jawLerp;
+
+    private void Update()
     {
-        candles = FindObjectsOfType<Candle>();
-    }
+        AnimateJaw();
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(grabKey))
+        if(Input.GetMouseButtonDown(1))
         {
-            if(heldCandle == null)
+            if (jawLerp >= jawLerpThreshold)
             {
-                GrabCandle();
-            }
-            else
-            {
-                heldCandle.SetCandle(throwSpeed, (grabPoint.forward + throwOffset).normalized);
-                heldCandle.transform.eulerAngles = new Vector3(0, heldCandle.transform.eulerAngles.y, 0);
-                heldCandle = null;
-            }
-        }
 
-        if(heldCandle != null)
-        {
-            heldCandle.transform.LookAt(boneReference);
-        }
-    }
-
-    private void GrabCandle()
-    {
-        List<Candle> candlesInRange = new List<Candle>();
-
-        // Get all candles within range 
-        foreach (Candle candle in candles)
-        {
-            if( (candle.distance = Vector3.Distance(candle.transform.position, grabPoint.position)) < range)
-            {
-                candlesInRange.Add(candle);
-            }
-        }
-
-        // Makes sure there is actually a candles within range 
-        if(candlesInRange.Count > 0)
-        {
-            // Find closest candle 
-            Candle closest = null;
-            foreach (Candle item in candlesInRange)
-            {
-                if ((closest == null || item.distance < closest.distance) && item.canBeGrabbed)
-                {
-                    closest = item;
-                }
-            }
-
-            heldCandle = closest;
-
-            if(heldCandle != null)
-            {
-                // At least one candle 
-                StartCoroutine(closest.GrabCandle(grabSpeed, grabPoint));
             }
         }
     }
 
-    private void OnDrawGizmos()
+    private void AnimateJaw()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(grabPoint.position, grabPoint.position + (grabPoint.forward + throwOffset).normalized * 4);
+        // On right click hold 
+        if (Input.GetMouseButton(1))
+        {
+            // Continues unless fully open 
+            if (neckLerp < 1)
+            {
+                neckLerp += neckOpenSpeed * Time.deltaTime;
+            }
+            if (jawLerp < 1)
+            {
+                jawLerp += JawOpenSpeed * Time.deltaTime;
+            }
+
+            // Applys the rotation 
+            neckJoint.targetRotation = Quaternion.Euler(Vector3.Lerp(neckDefaultRot, neckTargetRot, neckOpenAnimCurve.Evaluate(neckLerp)));
+            jawJoint.targetRotation = Quaternion.Euler(Vector3.Lerp(jawDefaultRot, jawTargetRot, jawOpenAnimCurve.Evaluate(jawLerp)));
+        }
+        else
+        {
+            // Continues unless fully closed 
+            if (neckLerp > 0)
+            {
+                neckLerp -= neckCloseSpeed * Time.deltaTime;
+            }
+            if (jawLerp > 0)
+            {
+                jawLerp -= neckOpenSpeed * Time.deltaTime;
+            }
+
+            // Applys the rotation 
+            neckJoint.targetRotation = Quaternion.Euler(Vector3.Lerp(neckDefaultRot, neckTargetRot, neckCloseAnimCurve.Evaluate(neckLerp)));
+            jawJoint.targetRotation = Quaternion.Euler(Vector3.Lerp(jawDefaultRot, jawTargetRot, jawCloseAnimCurve.Evaluate(jawLerp)));
+        }
     }
-    */
 }
