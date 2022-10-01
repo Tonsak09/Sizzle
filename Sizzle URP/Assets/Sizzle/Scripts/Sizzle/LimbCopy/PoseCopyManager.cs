7f6ea3cf3a8000373e4fb12140ad4aae0fe394d4
Narai Risser <naraiguy@gmail.com>
2022-10-01 19:58:29 -0400

    Animation works on button press

--- Sizzle URP/Assets/Sizzle/Scripts/Sizzle/LimbCopy/PoseCopyManager.cs
+++ Sizzle URP/Assets/Sizzle/Scripts/Sizzle/LimbCopy/PoseCopyManager.cs
@@ -4,8 +4,60 @@ using UnityEngine;
 
 public class PoseCopyManager : MonoBehaviour
 {
+    private Animator animator;
+    public AnimationClip clipTest;
 
-    [SerializeField] List<PoseCopy> body;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(AnimationCo(clipTest, null));
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Base Layer." + clipTest.name);
+            animator.Play(clipTest.name);
+        }
+    }
+
+    // Current issue is that we don't know what set of bones are currently being
+    // used by the animation so we need to remove them whenever an animation stops 
+
+    /// <summary>
+    /// Attaches all the bones necessary to the anim component by getting via
+    /// the keys. Then sends data to the animation manager as a coroutine 
+    /// </summary>
+    /// <param name="anim"></param>
+    /// <param name="keys"></param>
+    private void TryPoseAnimation(AnimationClip anim, string[] keys)
+    {
+        
+    }
+
+    /// <summary>
+    /// The coroutine that is passed to the animation manager. Animated
+    /// the pose copy and ends it when the animation is over. clearns up 
+    /// the animation from its mix of bones when done as well 
+    /// </summary>
+    /// <param name="anim"></param>
+    /// <param name="poseCopies"></param>
+    /// <returns></returns>
+    private IEnumerator AnimationCo(AnimationClip anim, PoseCopy[] poseCopies)
+    {
+        print(anim.name);
+       // animator.Play(anim.name);
+
+
+        yield return null;
+    }
+
+
+
+
+    // Old
+    /*[SerializeField] List<PoseCopy> body;
     [SerializeField] List<PoseCopy> legs;
 
     private Quaternion[] originalValuesBody;
@@ -39,12 +91,6 @@ public class PoseCopyManager : MonoBehaviour
         {
             body[i].UpdateTarget(originalValuesBody[i]);
         }
-    }
+    }*/
 
-
-
-
-
-    // needs to work via ienumators 
-    // Take in multiple animations using parts 
 }
