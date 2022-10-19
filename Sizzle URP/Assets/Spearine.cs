e6afb985ec777b9f498231fe99decb76cbaafd69
Narai Risser <naraiguy@gmail.com>
2022-10-18 20:35:22 -0400

    Brick and some spearine logic

--- Sizzle URP/Assets/Spearine.cs
+++ Sizzle URP/Assets/Spearine.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class Spearine : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] Transform rotationBone;
+
     [Header("Ranges")]
     [SerializeField] float closeRange;
     [SerializeField] float midRange;
     [SerializeField] float farRange;
 
+    [Header("Speeds")]
+    [SerializeField] float curiousTurnSpeed;
+    [SerializeField] float alertTurnSpeed;
+
+    [Header("Alertness")]
+    [SerializeField] float closeAlertRaise;
+    [SerializeField] float midAlertRaise;
+    [SerializeField] float farAlertRaise;
+
     private Transform player;
+    // Target could be a player, but also any other charged entity 
+    private Transform primaryTarget;
+
+    // Used to calculate how aware spearine is to all
+    // targets in its vicinity 
+    private Dictionary<Transform, float> alertnessDictionary;
+    private List<Transform> targets;
+
+    // If reaches 100 then Spearine is alert to target 
+    [Range(0, 100)]
+    private float alertness;
 
     private enum DistanceZone
     {
@@ -22,15 +45,33 @@ public class Spearine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        alertnessDictionary = new Dictionary<Transform, float>();
+
         // Gets player rather than Sizzle because Sizzle is a folder that doesn't represent the tru position 
         player = GameObject.FindWithTag("Player").transform;
+        alertnessDictionary.Add(player, 0);
 
+        // TESTING ONLY 
+        primaryTarget = player;
     }
 
     // Update is called once per frame
     void Update()
     {
         print(GetZone(player.position));
+
+        if(primaryTarget != null)
+        {
+            Vector3 targetVec = Vector3.ProjectOnPlane(primaryTarget.position - this.transform.position, this.transform.up);
+            this.transform.forward = Vector3.RotateTowards(this.transform.forward, targetVec, alertTurnSpeed * Time.deltaTime, 0.0f);
+        }
+        else
+        {
+            UpdateAlertness();
+
+            // Get target with largest alert 
+            // Curious rotate towards that 
+        }
     }
 
     /// <summary>
@@ -59,6 +100,40 @@ public class Spearine : MonoBehaviour
         {
             return DistanceZone.NotWithinRange;
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void UpdateAlertness()
+    {
+
+    }
+
+    /// <summary>
+    /// Spearine attacks the player via animation 
+    /// </summary>
+    private void Attack()
+    {
+        // Lunges head towards target according to range 
+    }
+
+    /// <summary>
+    /// Brings the player back and then resets the alertness
+    /// </summary>
+    private void ResetPosition()
+    {
+
+    }
+
+    /// <summary>
+    /// Whether or not the Spearine can see the target 
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    private bool CanDetect(Vector3 pos)
+    {
+        return true;
     }
 
     private void OnDrawGizmos()
